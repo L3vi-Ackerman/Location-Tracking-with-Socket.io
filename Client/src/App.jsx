8d26f5f1a55dad21c9545542d3c73a9be45b2108
L3vi Ackerman <l3vi3211@gmail.com>
2025-01-15 18:28:32 +0545

    updated login page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,24 +1,12 @@
 import React, { useState } from "react";
 import PersistentDrawerLeft from "./Component/Drawer/Drawer";
-import Login from './Component/Login/Login';
+import Login from "./Component/Login/Login";
 import "./App.css";
 
 const App = () => {
-  const [location, setlocation] = useState({
-    latitude: "27.7172",
-    longitude: "85.324",
-  });
-  const [login,setLogin] = useState(false);
+  const [login, setLogin] = useState(false);
 
-  return (
-    <>
-      {/* <MapComponent location={location} />
-      <UserListPanel /> */}
-      {
-        login ? <PersistentDrawerLeft />:<Login />
-      }
-    </>
-  );
+  return <>{login ? <PersistentDrawerLeft /> : <Login />}</>;
 };
 
 export default App;
--- Client/src/Component/Login/Login.jsx
+++ Client/src/Component/Login/Login.jsx
@@ -1,70 +1,96 @@
+import { useState } from "react";
 import Box from "@mui/material/Box";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-
+import Typography from "@mui/material/Typography";
 function Login() {
-    return (
+  const [formData, setformData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setformData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form Submitted:", formData);
+  };
+  return (
+    <Box
+      sx={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#f8f9fa",
+      }}
+    >
+      <Box
+        sx={{
+          width: "70%",
+          borderRadius: 1,
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          padding: 2,
+          boxShadow: 3,
+        }}
+      >
+        {/* Form on the Left */}
+        <Box sx={{ flex: 1, marginRight: 2, padding: 2 }}>
+          <Typography variant="h3" className="mb-3">
+            Welcome back!
+          </Typography>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email"name="email" placeholder="Enter email" value={formData.email} onChange={handleChange} />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" name="password" placeholder="Password" value={formData.password} onChange={handleChange}/>
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Box>
+
+        {/* Image on the Right */}
         <Box
           sx={{
-            width: "70%",
-            height:"100%",
-            height: "auto",
-            margin:"auto",
-            borderRadius: 1,
+            flex: 1,
             display: "flex",
-            justifyContent: "space-between",
+            justifyContent: "center",
             alignItems: "center",
-            padding: 2,
-            boxShadow: 3,
           }}
         >
-          {/* Form on the Left */}
-          <Box sx={{ flex: 1, marginRight: 2 }}>
-            <Form>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
-    
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-              </Form.Group>
-              <Button variant="primary" type="submit">
-                Submit
-              </Button>
-            </Form>
-          </Box>
-    
-          {/* Image on the Right */}
-          <Box
-            sx={{
-              flex: 1,
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
+          <img
+            src="./signup-image.jpg"
+            alt="Placeholder"
+            style={{
+              width: "100%",
+              maxWidth: "100%",
+              height: "100%",
+              borderRadius: "8px",
+              objectFit: "cover",
             }}
-          >
-            <img
-              src="./signup-image.jpg"
-              alt="Placeholder"
-              style={{
-                width: "100%",
-                maxWidth: "100%",
-                height:'100%',
-                borderRadius: "8px",
-                objectFit: "cover",
-              }}
-            />
-          </Box>
+          />
         </Box>
-      );
+      </Box>
+    </Box>
+  );
 }
 
 export default Login;
